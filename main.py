9f2ec0ef8527ab5860dd4d349517d424ee7c8e98
allison-0 <allisonulrich00@gmail.com>
2024-06-27 14:53:33 -0700

    Commit

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ def main():
     a1 = int(input('Enter your first number: '))
     a2 = int(input('Enter your second number: '))
     N = int(input('Enter the number of sequences: '))
-    result.append(a1)
-    result.append(a2)
-    for i in range(N-2):
-        num = result[-2] + result [-1]
-        result.append(num)
+    if N > 2:
+        result.append(a1)
+        result.append(a2)
+        for i in range(N-2):
+            num = result[-2] + result [-1]
+            result.append(num)
+        else:
+            print(result)
     else:
-        print(result)
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+        print("Please input more than two sequences")    
+        
 
     ########################################
     # Do not delete the return statement
