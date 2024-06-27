2310b872923c67ff9e76ec434682bee7d9f32f23
allison-0 <allisonulrich00@gmail.com>
2024-06-27 14:36:25 -0700

    commit

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
     N = int(input('Enter the number of sequences: '))
     result.append(a1)
     result.append(a2)
+    num = result[0] + result[-1]
+    for i in range(N):
+        num = result[-2] + result [-1]
+        result.append(num)
+        print(num)
     """
     ########################################
     Code Your Program here
